90918c769d162cb325e409567638574d61534602
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-03-03 20:55:35 -0800

    Test and fix atbash.c, move to folder

--- c-lang/atbash.c
+++ c-lang/atbash.c
@@ -11,18 +11,17 @@ Note:          Need to test, coded blind using windows notepad!
 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 char convertLetter(char ch);
 
 int main( int argc, char *argv[] ) {
+  int BUFFER_SIZE = 100;
+
   if (argc != 2) {
     printf("usage: %s <word to encode>\n\n", argv[0] ); 
     return 1;
   }
-
-  int BUFFER_SIZE = 100;
-
-
 
 /* How to validate the length of argv[1] isn't too long? Does this work?
   
@@ -40,25 +39,26 @@ int main( int argc, char *argv[] ) {
     strcpy(word, argv[1]);
 
     /* convert word one letter at a time until we reach null terminator */
-    while (word[i]) {
+    while (word[i] != 0) {
       /* Get next letter and convert to cipher */
       lttr = word[i];
       if (isalpha(lttr)) {
-        lettr = convertLetter(lttr);      
+        lttr = convertLetter(lttr);      
       }
 
       /* Copy each letter into the newWord */
-      newWord[i] = lttr;
+      newWord[i++] = lttr;
     }
+    newWord[i] = 0;
     
-    printf("Converted %s to %s.\n", argv[1], newWord);
+    printf("Converted \'%s\' to \'%s\'\n", argv[1], newWord);
   }
 
   return 0;  
 }
 
 char convertLetter(char ch) {
-  /* Set the ascii offset to get 'A' and 'a' to equal 0
+  /* Set the ascii offset to get 'A' and 'a' to equal 0 */
   int offset;
 
   if ( isupper(ch) ) {
