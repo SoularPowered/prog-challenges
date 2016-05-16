db76a84757b248343f468255be2bf0549831a341
Shawn Hillyer <shawn.hillyer@gmail.com>
2016-05-16 14:18:09 -0700

    Create leetcode-191.java

---
+++ leetcode-191.java
@@ -0,0 +1,17 @@
+/* 191. Number of 1 Bits
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+*/
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum = 0, mask = 1, numBits = 32;
+        for (int i = 0; i < numBits; i++) {
+            if ((n & mask) != 0)
+                sum++;
+            mask = mask << 1;
+        }
+        return sum;
+    }
+}
